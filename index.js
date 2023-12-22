501cf32653cdb534e86f8a2341888365609999e6
Jadson Rodrigues <jjadsonrodrigues45@gmail.com>
2023-12-22 14:55:00 -0300

    projeto Git lvl personagem

---
+++ index.js
@@ -0,0 +1,29 @@
+let nome = ("Flavio")
+console.log("Olá!" +" "+ nome)
+
+let xp = "130"
+
+if (xp < 1 === xp > 1000) {
+console.log("LVL FERRO")
+}
+  if (xp < 1001 === xp > 2000){
+    console.log("LVL BRONZE")    
+}
+if (xp < 2001 === xp > 5000){
+  console.log("LVL PRATA")    
+}
+if (xp < 5001 === xp > 7000){
+  console.log("LVL OURO")    
+}
+if (xp < 7001 === xp > 8000){
+  console.log("LVL PLATINA")    
+}
+if (xp < 8001 === xp > 9000){
+  console.log("LVL ASCENDENTE")    
+}
+if (xp < 9001 === xp > 10000){
+  console.log("LVL IMORTAL")
+}
+if  (xp >= 10001){
+  console.log("LVL RADIANTE")
+}
